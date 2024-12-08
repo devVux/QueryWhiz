ee5ad3ed60c6ef2ee4506812cfc1d223e407301d
Vullnet <vuvogli@gmail.com>
2024-12-08 20:32:54 +0100

    minor fixes

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,17 +1,14 @@
-from settings.config import Local, Staging
+from settings.config import Local, Production
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-config_space = os.getenv('CONFIG_SPACE', 'LOCAL').upper()
+config_space = os.getenv("CONFIG_SPACE", "LOCAL").upper()
 
-config_mapping = {
-    'LOCAL': Local,
-    'PRODUCTION': Production
-}
+config_mapping = {"LOCAL": Local, "PRODUCTION": Production}
 
 auto_config = config_mapping.get(config_space)
 
 if auto_config is None:
-    raise EnvironmentError(f'CONFIG_SPACE is an unexpected value: {config_space}')
+    raise EnvironmentError(f"CONFIG_SPACE is an unexpected value: {config_space}")
